4048b2f8b942ad0425b433db316920a868d3973b
Brenton Unger <brenton.s.unger@live.com>
2022-12-22 23:44:04 -0800

    set to home to screen instead of view

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -3,8 +3,8 @@ import 'package:flutter/material.dart';
 
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({Key? key, required this.themeService}) : super(key: key);
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key, required this.themeService}) : super(key: key);
 
   static const routeName = '/';
 
